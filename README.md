f98d15e5918ddeaf75f4dbd37ed047a4d0b995c6
Peter Schlamp <p.schlamp@westernhealth.com>
2020-06-12 09:20:33 -0700

    added plugin and readme comment on it

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # learn-wordpress-coding-docker
+
+Web apps in wordpress involve mostly coding but to associated custom post types together we use a 
+plug-in:
+
+Advanced Custom Fields
+	
+
+Customize WordPress with powerful, professional and intuitive fields.
+| By Elliot Condon | View details
+
+################################################################################################
+
--- wp-content/mu-plugins/namethisanything.php
+++ wp-content/mu-plugins/namethisanything.php
@@ -6,9 +6,13 @@ function my_remove_menu_pages() {
 	remove_menu_page( 'upload.php' );                 //Media
 	remove_menu_page( 'edit-comments.php' );          //Comments
 	remove_menu_page( 'themes.php' );                 //Appearance
-	remove_menu_page( 'users.php' );                  //Users
+	//remove_menu_page( 'users.php' );                  //Users
 	remove_menu_page( 'tools.php' );                  //Tools
-	remove_menu_page( 'options-general.php' );        //Settings
+	remove_menu_page('edit.php?post_type=page');
+	
+	//add the below when in production
+	//remove_menu_page( 'options-general.php' );        //Settings
+	//remove_menu_page( 'plugins.php' );
 }
 
 
