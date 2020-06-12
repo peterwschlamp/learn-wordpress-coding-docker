03c64cd39676bfe7efdc929ec18f74bb9bce4c9f
Peter Schlamp <p.schlamp@westernhealth.com>
2020-06-12 08:04:04 -0700

    learned how to remove menu items from admin

--- wp-content/mu-plugins/namethisanything.php
+++ wp-content/mu-plugins/namethisanything.php
@@ -1,6 +1,19 @@
 <?php
 
-function universe_post_types() {
+add_action( 'admin_menu', 'my_remove_menu_pages' );
+function my_remove_menu_pages() {
+	remove_menu_page( 'edit.php' );                   //Posts
+	remove_menu_page( 'upload.php' );                 //Media
+	remove_menu_page( 'edit-comments.php' );          //Comments
+	remove_menu_page( 'themes.php' );                 //Appearance
+	remove_menu_page( 'users.php' );                  //Users
+	remove_menu_page( 'tools.php' );                  //Tools
+	remove_menu_page( 'options-general.php' );        //Settings
+}
+
+
+function my_init() {
+
 	register_post_type('event', array(
 		'rewrite' => array(
 			'slug' => 'events'
@@ -21,8 +34,31 @@ function universe_post_types() {
 		),
 		'menu_icon' => 'dashicons-calendar'
 	));
+
+	register_post_type('program', array(
+		'rewrite' => array(
+			'slug' => 'programs'
+		),
+		'has_archive' => true,
+		'public' => true,
+		'supports' => array(
+			'title',
+			'editor',
+			'custom-fields'
+		),
+		'labels' => array(
+			'name' => 'Programs',
+			'add_new_item' => 'Add New Program',
+			'edit_item' => 'Edit Program',
+			'all_items' => 'All Programs',
+			'singular_name' => 'Program'
+		),
+		'menu_icon' => 'dashicons-awards'
+	));
 }
 
-add_action('init', 'universe_post_types')
+
+
+add_action('init', 'my_init')
 
 ?>
